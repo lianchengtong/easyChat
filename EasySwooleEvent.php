29f5e2a5a14d44f1a4df80df2f1e7de7b2056cbc
runstp <runstp@163.com>
2018-06-21 00:38:28 +0800

    :sparkles: 注册全局Db Config

--- EasySwooleEvent.php
+++ EasySwooleEvent.php
@@ -10,21 +10,27 @@ namespace EasySwoole;
 
 use \EasySwoole\Core\AbstractInterface\EventInterface;
 use \EasySwoole\Core\Swoole\ServerManager;
+use \EasySwoole\Core\Swoole\EventHelper;
 use \EasySwoole\Core\Swoole\EventRegister;
+use \EasySwoole\Core\Swoole\Process\ProcessManager;
 use \EasySwoole\Core\Http\Request;
 use \EasySwoole\Core\Http\Response;
+use \EasySwoole\Core\Component\Di;
+
+use \think\Db;
 
 Class EasySwooleEvent implements EventInterface {
 
     public static function frameInitialize(): void
     {
-        // TODO: Implement frameInitialize() method.
         date_default_timezone_set('Asia/Shanghai');
+
+        // 全局初始化Db类
+        Db::setConfig(Config::getInstance()->getConf('DATABASE'));
     }
 
     public static function mainServerCreate(ServerManager $server,EventRegister $register): void
     {
-        // TODO: Implement mainServerCreate() method.
     }
 
     public static function onRequest(Request $request,Response $response): void
