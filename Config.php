fab3824c50fe9d7e1df26a0c66f28b0352f2e3ee
runstp <runstp@163.com>
2018-06-21 00:36:39 +0800

    :art: 增加了DATABASE配置 之后不再提交Config

--- Config.php
+++ Config.php
@@ -46,6 +46,26 @@ return [
             'type' => 1
         ]
     ],
+    'DATABASE' => [
+        // 数据库类型
+        'type'            => 'mysql',
+        // 服务器地址
+        'hostname'        => '',
+        // 数据库名
+        'database'        => '',
+        // 用户名
+        'username'        => '',
+        // 密码
+        'password'        => '',
+        // 端口
+        'hostport'        => '',
+        // 数据库表前缀
+        'prefix'          => 'chat_',
+        // 是否需要断线重连
+        'break_reconnect' => true,
+        // 自动时间戳
+        'auto_timestamp' => true,
+    ],
     'REDIS' => [
         // redis主机地址
         'host' => '127.0.0.1',
